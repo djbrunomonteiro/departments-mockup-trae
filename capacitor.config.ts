3c3e11c6a7073c69e88ce7910d22d0e77a98f212
djbrunomonteiro <brunomonteiroestudio@gmail.com>
2025-07-30 15:32:35 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'departments-mockup',
+  webDir: 'www'
+};
+
+export default config;
